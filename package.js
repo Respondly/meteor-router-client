93135506902c195d34d6209618ef1a0b090e5efa
Phil Cockfield <phil@cockfield.net>
2014-12-04 13:38:07 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'respondly:router-client',
   summary: 'Simple client-side routing',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/Respondly/meteor-router-client.git'
 });
 
@@ -12,8 +12,8 @@ Package.onUse(function (api) {
   api.use(['coffeescript', 'http']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
   api.use('respondly:css-stylus@1.0.3');
-  api.use('respondly:ctrl@1.0.0');
-  api.use('respondly:util@1.0.0');
+  api.use('respondly:ctrl@1.0.1');
+  api.use('respondly:util@1.0.1');
   api.export('Router');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
