e7d6ce78ae6774289c46c07cebc8f78cbc34844e
Zoltan Olah <zol@percolatestudio.com>
2015-05-11 12:53:44 -0700

    Adds test hack for when app is served within optimizely’s proxy

--- client/libs/page-js.js
+++ client/libs/page-js.js
@@ -139,6 +139,14 @@ THE SOFTWARE.
     if (false !== options.click) window.addEventListener('click', onclick, false);
     if (!dispatch) return;
     var url = location.pathname + location.search + location.hash;
+
+    // When running inside the optimizely editor, path will be something like:
+    // "/http://static-test.percolatestudio.com/?optimizely_disable=true..."
+    //
+    // This regexp strips the start back to /?...
+    url = url.replace(/^\/https?\:\/\/.*?\//, '/');
+    console.log('page.start url,', url);
+
     page.replace(url, null, true, dispatch);
   };
 
